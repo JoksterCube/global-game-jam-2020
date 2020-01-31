5583b1017fe69b8d7976fad07203209137d61432
Joogurtas <60510518+Joogurtas@users.noreply.github.com>
2020-01-31 23:09:10 +0200

    Click registration script

---
+++ UnityProject/Assets/Scripts/ClickRegistration.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickRegistration : MonoBehaviour
+{
+    public Camera cam;
+    public Vector2 Coordinates()
+    {
+        var mousePos = Input.mousePosition;
+        Vector2 p = cam.ScreenToWorldPoint(mousePos);
+
+        return p;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Koordinatės:"+Coordinates());
+        }
+    }
+}

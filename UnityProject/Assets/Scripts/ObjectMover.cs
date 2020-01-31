f5165193dfed332e356566ec934829ddacba8eb0
JoksterCube <jokubrus@hotmail.com>
2020-02-01 00:33:59 +0200

    Added Waves

--- UnityProject/Assets/Scripts/ObjectMover.cs
+++ UnityProject/Assets/Scripts/ObjectMover.cs
@@ -9,10 +9,20 @@ public class ObjectMover : MonoBehaviour
 
     public float force = 10;
     public float range = 10;
-    //public int rippleCount = 3;
+    public int rippleCount = 3;
+    public float rippleDelay = 1;
+
+    public Wave prefab;
 
     public void StartWaveAtPosition(Vector2 position)
     {
-            waveDelegate(position, force, range);
+        Wave wave = Instantiate(prefab, position, Quaternion.identity, transform) as Wave;
+        wave.RippleCount = rippleCount;
+        wave.RippleDelay = rippleDelay;
+    }
+
+    public void Wave(Vector2 position)
+    {
+        waveDelegate(position, force, range);
     }
 }

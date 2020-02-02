5c7664806199a0169ad1456d0be7a0277085b437
JoksterCube <jokubrus@hotmail.com>
2020-02-02 15:12:57 +0200

    Hearts / Danger change

--- UnityProject/Assets/Scripts/Managers/LifeTracker.cs
+++ UnityProject/Assets/Scripts/Managers/LifeTracker.cs
@@ -6,17 +6,21 @@ using UnityEngine.UI;
 [RequireComponent(typeof(GameStateTracker))]
 public class LifeTracker : MonoBehaviour
 {
+    public Image dangerWrapper;
     public Image dangerIndicator;
 
     private GameStateTracker gameStateTracker;
 
-    [Range(0,1)]
+    public float fadeTime = 1;
+
+    [Range(0, 1)]
     public float regenerationSpeed = .1f;
     [Range(0, 1)]
     public float losingSpeed = .2f;
 
     public float delayUntilRegeneration = 1;
 
+    private float currentFade = 0;
     private float currentDanger = 0;
     private float nextRegeneration = 0;
     private bool hasHappenedThisFixedFrame = false;
@@ -32,8 +36,21 @@ public class LifeTracker : MonoBehaviour
                 currentDanger -= regenerationSpeed * Time.deltaTime;
         }
         currentDanger = Mathf.Clamp01(currentDanger);
-        dangerIndicator.fillAmount = currentDanger;
+        ((RectTransform)dangerIndicator.transform).localScale = (1 - currentDanger) * Vector3.one;
     }
+
+    private void LateUpdate()
+    {
+        float step = 1 / fadeTime * Time.deltaTime;
+        if (currentDanger <= 0)
+            currentFade = Mathf.Clamp01(currentFade - step);
+        else if (currentDanger > 0)
+            currentFade = Mathf.Clamp01(currentFade + step);
+
+        dangerWrapper.color = new Color(dangerWrapper.color.r, dangerWrapper.color.g, dangerWrapper.color.b, currentFade);
+        dangerIndicator.color = new Color(dangerIndicator.color.r, dangerIndicator.color.g, dangerIndicator.color.b, currentFade);
+    }
+
     private void FixedUpdate()
     {
         hasHappenedThisFixedFrame = false;

a0b7e587903787598525a23be4f47c3d9451747f
JoksterCube <jokubrus@hotmail.com>
2020-02-02 11:02:48 +0200

    Simple Generation Done

--- UnityProject/Assets/Scripts/Managers/GenerateSidescrollerWorld.cs
+++ UnityProject/Assets/Scripts/Managers/GenerateSidescrollerWorld.cs
@@ -7,7 +7,10 @@ public class GenerateSidescrollerWorld : MonoBehaviour
 {
     public Transform target;
     public List<GameObject> trapPrefabs;
+    public List<GameObject> wallPrefabs;
     public Vector2 borderBoundaries;
+    public float wallOffset = -1;
+    public float wallUnitLength = 15;
 
     public float generationDistance = 30;
     public float generationCheckDistance = 10;
@@ -17,28 +20,29 @@ public class GenerateSidescrollerWorld : MonoBehaviour
     public float noiseScale = 2;
 
     public Transform trapParrent;
+    public Transform wallParent;
 
-    private Queue<Vector2> queue;
+    private Queue<Vector2> trapQueue;
     private float nextSpawnFromX = 0;
 
     private void Awake()
     {
-        queue = new Queue<Vector2>();
+        trapQueue = new Queue<Vector2>();
     }
 
     private void Update()
     {
-        if (queue is null || queue.Count <= 0)
+        if (trapQueue is null || trapQueue.Count <= 0)
             return;
         do
         {
-            Vector2 position = queue.Dequeue();
+            Vector2 position = trapQueue.Dequeue();
             int index = Random.Range(0, trapPrefabs.Count);
             float zAngle = Random.Range(-180, 180);
             Quaternion randomZRotation = Quaternion.Euler(0, 0, zAngle);
 
             GameObject trap = Instantiate(trapPrefabs[index], position, randomZRotation, trapParrent ?? transform) as GameObject;
-        } while (queue.Count > 0);
+        } while (trapQueue.Count > 0);
     }
     private void FixedUpdate()
     {
@@ -46,26 +50,41 @@ public class GenerateSidescrollerWorld : MonoBehaviour
 
         if (target.position.x >= nextSpawnFromX - generationCheckDistance)
         {
-            StartNewGeneratio();
+            StartNewGeneration();
         }
     }
-    public void StartNewGeneratio()
+    public void StartNewGeneration()
     {
         Thread t = new Thread(GenerateChunk);
         t.Start(nextSpawnFromX);
+        GenerateWalls(nextSpawnFromX);
         nextSpawnFromX += generationDistance;
+    }
+
+    public void GenerateWalls(float x)
+    {
+        for (int i = 1; i <= generationDistance / (wallUnitLength * 2); i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                int index = Random.Range(0, wallPrefabs.Count);
+                Vector2 position = new Vector2(x + i * wallUnitLength * 2, (borderBoundaries[j] - Mathf.Pow((-1), j) * wallOffset));
+                Quaternion rotation = Quaternion.Euler(0, 0, 90 * Mathf.Pow((-1), j));
+                Instantiate(wallPrefabs[index], position, rotation, wallParent);
+            }
+        }
     }
 
     private void GenerateChunk(System.Object obj)
     {
         float startingX = (float)obj;
-        for (float x = startingX; x < nextSpawnFromX; x += checkStep)
+        for (float x = startingX; x < startingX + generationDistance; x += checkStep)
         {
             for (float y = borderBoundaries.x; y <= borderBoundaries.y; y += checkStep)
             {
                 float noiseValue = Mathf.PerlinNoise(x * noiseScale, y * noiseScale);
                 if (noiseValue >= spawnThreshold)
-                    queue.Enqueue(new Vector2(x, y));
+                    trapQueue.Enqueue(new Vector2(x, y));
             }
         }
     }
